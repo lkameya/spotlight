00f71789ea67655e83ae481a190418d3e0b63a1d
Leonardo Kameya <leonardo.kameya@gmail.com>
2019-03-19 03:26:21 -0400

    Config heroku

---
+++ client/build/precache-manifest.6cfdfd5a12035fe4cd79a313cb40ea1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "87aa26e7e56123789a11",
+    "url": "/static/js/main.462f0c89.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5f8f50dd0c4136e826f",
+    "url": "/static/js/2.b29423b5.chunk.js"
+  },
+  {
+    "revision": "bd5996cf61ac151836c03ca526ca4991",
+    "url": "/index.html"
+  }
+];
