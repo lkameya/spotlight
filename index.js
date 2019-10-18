a20ef1a857d552512bf1766c0eb5cecdb7d43b4b
Leonardo Kameya <leonardo.kameya@gmail.com>
2019-10-18 15:46:44 -0400

    Adding new docker file

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const generateRandomString = function (length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (let i = 0; i < length; i++) {
+  for (var i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
